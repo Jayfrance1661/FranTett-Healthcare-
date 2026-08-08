471b5c9fb3052e355e2529b4d69a8393e9ea717a
TO SOP13 <tosop13@lindfastgrp.com>
2026-08-08 16:22:42 -0400

    Continue appointment API development

--- FRANTETT/server.js
+++ FRANTETT/server.js
@@ -1,26 +1,57 @@
 const cors = require("cors");
-
 const express = require("express");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 const PORT = 3000;
 
 // Home page
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to FranTett Healthcare Backend!</h1>");
+    res.send("Welcome to FranTett Healthcare Backend!");
 });
 
 // Doctors API
 app.get("/api/doctors", (req, res) => {
 
-    app.post("/api/appointments", (req, res) => {
+    const doctors = [
+        {
+            id: 1,
+            name: "Dr. Francis Tetteh",
+            specialty: "Naturopathic Doctor"
+        },
+        {
+             id: 2,
+            name: "Dr. Talent",
+            specialty: "Naturopathic Doctor"
+        },
+        {
+            id: 3,
+            name: "Dr. Emily Brown",
+            specialty: "Pediatrician"
+        },
+        {
+            id: 3,
+            name: "Dr. Sam",
+            specialty: "Cardiologist"
+        }
+    ];
 
-    const appointment = req.body;
+    res.json(doctors);
+
+});
+
+// Appointments
+const appointments = [];
+
+app.post("/api/appointments", (req, res) => {
+
+    const appointment = {
+    id: appointments.length + 1,
+    ...req.body
+};
 
     appointments.push(appointment);
 
@@ -32,28 +63,14 @@ app.get("/api/doctors", (req, res) => {
     });
 
 });
-    const doctors = [
-        {
-            id: 1,
-            name: "Dr. Francis Tetteh",
-            specialty: "Family Physician"
-        },
-        {
-            id: 2,
-            name: "Dr. Emily Brown",
-            specialty: "Pediatrician"
-        },
-        {
-            id: 3,
-            name: "Dr. Sam Johnson",
-            specialty: "Cardiologist"
-        }
-    ];
 
-    res.json(doctors);
+app.get("/api/appointments", (req, res) => {
+
+    res.json(appointments);
+
 });
 
-const appointments = [];
+// Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
